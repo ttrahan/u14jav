6d48003d89594b11f0838f3da19d59c24fc7e4f1
ankul-shippable <ankul@shippable.com>
2016-02-10 13:01:48 +0530

    #13- added heading

--- test/_java.sh
+++ test/_java.sh
@@ -5,6 +5,7 @@ echo "update-alternatives --list java"
 update-alternatives --list java
 printf "\n\n"
 
+echo "==================== Switching between versions ========================"
 echo "update-alternatives --set java /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/java"
 update-alternatives --set java /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/java
 printf "\n"
