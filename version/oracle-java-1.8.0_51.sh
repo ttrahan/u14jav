e521c35584a19bb539a77135e374354af7bfee0a
Tom Trahan <tom@shippable.com>
2016-07-18 18:13:34 -0700

    add install scripts for 8.51 and 8.92

--- version/oracle-java-1.8.0_51.sh
+++ version/oracle-java-1.8.0_51.sh
@@ -2,7 +2,6 @@
 
 echo "================ Installing oracle-java8-minor-release ================="
 echo oracle-java8-minor-release shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-# mkdir -p /opt/jdk
 mkdir -p /usr/lib/jvm/oracle-java-1.8.0_51
 cd /usr/lib/jvm
 wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux-x64.tar.gz
--- version/oracle-java-1.8.0_92.sh
+++ version/oracle-java-1.8.0_92.sh
@@ -2,7 +2,6 @@
 
 echo "================ Installing oracle-java8-minor-release ================="
 echo oracle-java8-minor-release shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-# mkdir -p /opt/jdk
 mkdir -p /usr/lib/jvm/oracle-java-1.8.0_92
 cd /usr/lib/jvm
 wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
